e741c16dae58b117a8be8f7d261d87ffcd327608
mratsag <mrat.sag@hotmail.com>
2026-03-24 00:16:18 +0300

    feat: add transactional annotation to refresh token method in AuthService

--- src/main/java/com/muratsag/kafkaboard/auth/AuthService.java
+++ src/main/java/com/muratsag/kafkaboard/auth/AuthService.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -54,6 +55,7 @@ public class AuthService {
         return buildAuthResponse(user);
     }
 
+    @Transactional
     public AuthResponse refresh(RefreshTokenRequest request) {
         RefreshTokenEntity refreshToken = refreshTokenService.rotateRefreshToken(request.getRefreshToken());
 
